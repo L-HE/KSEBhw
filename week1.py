2cb40e905f7d6c1592581146b2eb28c7d7986813
LHE <mekakusira@gmail.com>
2025-03-14 22:09:01 +0900

    v0.2 week1_3 #10430

--- week1.py
+++ week1.py
@@ -1,3 +1,10 @@
-# Baekjoon 2557 Hello World
+# Baekjoon 10430 나머지
 
-print(f"Hello World!")
+A, B, C = map(int, input().split())
+
+ans1 = (A + B) % C
+ans2 = ((A % C) + (B % C)) % C
+ans3 = (A * B) % C
+ans4 = ((A % C) * (B % C)) % C
+
+print(f"{ans1}\n{ans2}\n{ans3}\n{ans4}\n")
