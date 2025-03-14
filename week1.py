b9877351cef0588286c8223cbbe279ddcc0d9d93
LHE <mekakusira@gmail.com>
2025-03-14 22:06:46 +0900

    v0.2 week1_2 #2557

--- week1.py
+++ week1.py
@@ -1,12 +1,3 @@
-# Baekjoon 1000 A+B
+# Baekjoon 2557 Hello World
 
-def add(a, b) -> int:
-  sum = a + b
-  return sum
-
-a, b = map(int, input().split())
-
-if 0 < a < 10 and 0 < b < 10:
-    print(add(a, b))
-else:
-    print("Out of range.")
+print(f"Hello World!")
